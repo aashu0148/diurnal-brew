fa86b34612ae7ec5ab14eedd98f70ede0cd699f7
aashu <buildforfb@gmail.com>
2026-09-05 09:08:32 +0530

    sanity: homepage points at this public repo, not the private source

--- Casks/diurnal.rb
+++ Casks/diurnal.rb
@@ -5,7 +5,7 @@ cask "diurnal" do
   url "https://github.com/aashu0148/diurnal-brew/releases/download/v#{version}/Diurnal-#{version}.dmg"
   name "Diurnal"
   desc "Capture-and-plan overlay for macOS"
-  homepage "https://github.com/aashu0148/diurnal"
+  homepage "https://github.com/aashu0148/diurnal-brew"
 
   depends_on macos: ">= :sequoia"
 
