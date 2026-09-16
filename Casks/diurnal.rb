1dfc92de9c601e9fe8cafb3ac88cc6b02a28b719
aashu <buildforfb@gmail.com>
2026-09-16 11:44:44 +0530

    diurnal 0.2.5

--- Casks/diurnal.rb
+++ Casks/diurnal.rb
@@ -1,6 +1,6 @@
 cask "diurnal" do
-  version "0.2.4"
-  sha256 "0ff505a9b90d7016a4314a2fe9612bdbedcdcab332fbe235991e611dffc79edc"
+  version "0.2.5"
+  sha256 "e5ab3ee5d4729a51c6f9e4d0fbf0310c6bd9220abbb4575131867c45621201bb"
 
   url "https://github.com/aashu0148/diurnal-brew/releases/download/v#{version}/Diurnal-#{version}.dmg"
   name "Diurnal"
